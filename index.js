bffa168b1536a0f61665bbe783b9800bc5257011
alyona26o2 <alenag2602@gmai.com>
2023-01-02 13:47:25 +0200

    Lab 5 added

--- index.js
+++ index.js
@@ -4,6 +4,7 @@ const path = require('path');
 const auth0 = require('auth0');
 require('dotenv').config();
 const jwt = require('jsonwebtoken');
+const {auth} = require('express-oauth2-jwt-bearer');
 const port = 3000;
 
 const app = express();
@@ -11,7 +12,6 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended: true}));
 
 const SESSION_KEY = 'Authorization';
-
 
 const AuthenticationClient = new auth0.AuthenticationClient(
     {
@@ -31,13 +31,15 @@ const ManagementClient = new auth0.ManagementClient(
 
 app.use(async (req, res, next) => {
     let authorization = req.get(SESSION_KEY);
+    console.log(authorization)
+    let refresh = req.get('Refresh');
 
     if (authorization) {
         let tokens = authorization.split(';');
         if (tokens.length === 3) {
             req.access_token = tokens[0];
             req.refresh_token = tokens[1];
-            req.id_token = tokens[2];
+            //req.id_token = tokens[2];
         }
         try {
             jwt.verify(req.access_token,  process.env.CLIENT_SECRET, async function(err, decoded) {
@@ -51,29 +53,25 @@ app.use(async (req, res, next) => {
                     );
 
                     req.access_token = refreshRequest.access_token;
-                    req.id_token = refreshRequest.id_token;
 
-                    console.log("refreshed", refreshRequest);
+                    console.log("Token was refreshed", refreshRequest);
                 }
             });
         } catch (err) {
             res.status(401).send();
             return;
         }
-
-        res.headers = {Authorization: `${req.access_token};${req.refresh_token};${req.id_token}`}
+        res.headers = {Authorization: `${req.access_token};${req.refresh_token}`}
     }
-
     next();
-})
-;
+});
 
 app.get('/', (req, res) => {
     if (req.access_token) {
-        let payload = jwt.decode(req.id_token);
+        let payload = jwt.decode(req.access_token);
 
         return res.json({
-            username: payload.name,
+            username: payload.sub,
             logout: 'http://localhost:3000/logout'
         })
     }
@@ -95,17 +93,27 @@ app.post('/api/login', async (req, res) => {
         loginResult = await auth0Login(login, password);
     } catch (err) {
         res.status(401).send();
-
+        console.log(err);
         return;
     }
     console.log(loginResult);
 
     res.json({
         access_token: loginResult.access_token,
-        refresh_token: loginResult.refresh_token,
-        id_token: loginResult.id_token
+        refresh_token: loginResult.refresh_token
     });
 });
+
+const checkJwt = auth({
+    audience: 'https://kpi.eu.auth0.com/api/v2/',
+    issuerBaseURL: `https://kpi.eu.auth0.com`,
+});
+
+app.get('/api/check', checkJwt, (req, res) => {
+    res.json({
+        message: 'Authentication is successful'
+    })
+})
 
 
 app.post('/api/register', async (req, res) => {
@@ -117,11 +125,9 @@ app.post('/api/register', async (req, res) => {
             {
                 email: email,
                 password: password,
-                email_verified: false,
                 connection: 'Username-Password-Authentication'
             }
         );
-        console.log(createResult);
 
         if (createResult instanceof Error) {
             throw createResult;
@@ -129,16 +135,15 @@ app.post('/api/register', async (req, res) => {
     } catch (err) {
         res.status(400).send();
         console.log(err);
-
         return;
     }
-
     console.log(createResult)
     res.status(200).send();
 });
 
 async function auth0Login(login, password) {
     const data = {
+        audience: 'https://kpi.eu.auth0.com/api/v2/',
         client_id: 'JIvCO5c2IBHlAe2patn6l6q5H35qxti0',
         username: login,
         password: password,
